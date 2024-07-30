d986a3fd2106fd084490e54576bb03a879ddb85b
Kern000 <holupkern@gmail.com>
2024-07-30 17:43:22 +0800

    preventing inheritance and override via final

--- PolyA.java
+++ PolyA.java
@@ -1,11 +1,19 @@
 public class PolyA {
 
+    final int num = 8; // cannot reassign to num
+    // final int num2; // this will not work;
+
     public void show(){
         System.out.println("In PolyA");
     }
 
+    public final void show2(){  //no one can inherit it
+        System.out.println("Final show2 in PolyA");
+    }
+
     public static void main(String a[]){
 
+        final int num = 10;
         PolyA obj1 = new PolyA();
         obj1.show();
         obj1 = new PolyB();
@@ -13,6 +21,10 @@ public class PolyA {
         obj1 = new PolyC();
         obj1.show();
         // This is dynamic method dispatch
+        obj1.show2();
+
+        System.out.println(obj1.num);
+        System.out.println(num);
     }
     
 }
@@ -23,6 +35,10 @@ class PolyB extends PolyA{
     public void show(){
         System.out.println("In PolyB");
     }
+
+    // public void show2(){
+    // }
+    // Cannot override because of final
 
 }
 
